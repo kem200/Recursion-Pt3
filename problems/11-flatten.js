50cd7d2627c8882aa886d7d556a7642a2812e005
Big-Hdub <hdub-bb@hotmail.com>
2023-10-19 20:34:03 -0700

    problem 11 solved

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -7,15 +7,28 @@ Do not use the built in Array.flat.
 
 Examples:
 
-flatten([]); // []
-flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
+
 ***********************************************************************/
 
-function flatten(arr) {
-  // Your code here 
+const flatten = (arr) => {
+  if (arr.length === 0) return [];
+  if (Array.isArray(arr[0])) return flatten(arr[0]).concat(flatten(arr.slice(1)));
+  else return [arr[0]].concat(flatten(arr.slice(1)));
 }
-  
+
+// function flatten(arr) {
+//   if (!Array.isArray(arr)) return [];
+//   if (arr.length > 0) {
+//     let res = [arr.shift()].concat(...flatten(...arr))
+//     return res
+//   }
+//   return arr
+// }
+debugger
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]    [1 [2 [3]]]  [1 2 [3]]  [1  2  3]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
